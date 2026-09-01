b79831d0801dd6ac90001b6e738d9b89be4dff77
kenner costa <kenner.costa@outlook.com>
2026-09-01 17:39:04 -0300

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const app = require('./app');
+const os = require('os');
+
+function getLocalIP() {
+  const interfaces = os.networkInterfaces();
+
+  for (const name of Object.keys(interfaces)) {
+    for (const net of interfaces[name]) {
+      if (net.family === 'IPv4' && !net.internal) {
+        return net.address;
+      }
+    }
+  }
+
+  return 'localhost';
+}
+
+const PORT = process.env.PORT || 3000;
+const IP = getLocalIP();
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Local: http://localhost:${PORT}`);
+  console.log(`🌐 Rede:  http://${IP}:${PORT}`);
+});
